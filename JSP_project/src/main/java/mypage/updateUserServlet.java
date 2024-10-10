e7671c36ed43ad1b9ca89532d95daaf57c183394
parkherooo <dlzlrhffma1@naver.com>
2024-10-10 12:25:42 +0900

    a
    a

--- JSP_project/src/main/java/mypage/updateUserServlet.java
+++ JSP_project/src/main/java/mypage/updateUserServlet.java
@@ -13,59 +13,54 @@ import javax.servlet.http.HttpSession;
 import user.UserBean;
 import user.UserMgr;
 
-
 @WebServlet("/mypage/updateUser")
 public class updateUserServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		String action = request.getParameter("action");
-		UserBean bean = new UserBean();
-		
-		bean.setName(request.getParameter("name"));
-		bean.setUserId(request.getParameter("userId"));
-		bean.setPhone(request.getParameter("phone"));
-		bean.setAddress(request.getParameter("address"));
-		bean.setBirth(request.getParameter("birth"));
-		
-		UserMgr mgr = new UserMgr();
-		
-		if(action.equals("update")) {
-			if(mgr.myinfoUpdate(bean)) {
-				PrintWriter out = response.getWriter();
-				 out.println("<script>");
-				 out.println("alert('수정 성공했습니다.')");
-				 out.println("</script>");
-				response.sendRedirect("myPage.jsp");
-			} else {
-				response.setContentType("text/html; charset=UTF-8");
-				 PrintWriter out = response.getWriter();
-				 out.println("<script>");
-				 out.println("alert('수정 실패했습니다.')");
-				 out.println("history.back()");
-				 out.println("</script>");
-			}
-		} else if(action.equals("delete")) {
-			if(mgr.myinfoDelete(bean.getUserId())) {
-				PrintWriter out = response.getWriter();
-				 out.println("<script>");
-				 out.println("alert('탈퇴 성공했습니다.')");
-				 out.println("</script>");
-				 HttpSession session = request.getSession();
-				 session.invalidate(); // 세션을 무효화하여 모든 속성을 삭제합니다.
-				response.sendRedirect("../main/main.jsp");
-			} else {
-				response.setContentType("text/html; charset=UTF-8");
-				 PrintWriter out = response.getWriter();
-				 out.println("<script>");
-				 out.println("alert('탈퇴 실패했습니다.')");
-				 out.println("history.back()");
-				 out.println("</script>");
-			}
-		}
-		
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String action = request.getParameter("action");
+        UserBean bean = new UserBean();
 
+        bean.setName(request.getParameter("name"));
+        bean.setUserId(request.getParameter("userId"));
+        bean.setPhone(request.getParameter("phone"));
+        bean.setAddress(request.getParameter("address"));
+        bean.setBirth(request.getParameter("birth"));
+
+        UserMgr mgr = new UserMgr();
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        if ("update".equals(action)) {
+            if (mgr.myinfoUpdate(bean)) {
+                out.println("<script>");
+                out.println("alert('수정 성공했습니다.');");
+                out.println("location.href = '../mypage/myPage.jsp';");  // JS로 리다이렉트
+                out.println("</script>");
+            } else {
+                out.println("<script>");
+                out.println("alert('수정 실패했습니다.');");
+                out.println("history.back();");
+                out.println("</script>");
+            }
+        } else if ("delete".equals(action)) {
+            if (mgr.myinfoDelete(bean.getUserId())) {
+                HttpSession session = request.getSession();
+                session.invalidate(); // 세션 무효화
+
+                out.println("<script>");
+                out.println("alert('탈퇴 성공했습니다.');");
+                out.println("location.href = '../main/main.jsp';");  // JS로 리다이렉트
+                out.println("</script>");
+            } else {
+                out.println("<script>");
+                out.println("alert('탈퇴 실패했습니다.');");
+                out.println("history.back();");
+                out.println("</script>");
+            }
+        }
+        out.flush();  // 출력 버퍼를 비움
+        out.close();  // 출력 스트림을 닫음
+    }
 }
